806ae8f8e7b277da55a1f943738e1f13c84f89e3
yuichiroTCY <t.yic.yt@gmail.com>
2016-09-11 14:56:21 +0900

    Modify frontend to show error

--- src/js/app.js
+++ src/js/app.js
@@ -17,18 +17,26 @@ new Vue({
     result: {},
     isFetching: false,
     isFailed: null,
+    error: null,
   },
   methods: {
     requestClassify: function() {
       this.isFetching = true;
       this.$http.post(classifyApi, {content: this.content}).then((response) => {
         // success callback
-        this.result = response.body.result;
+        if (response.body.ok) {
+          this.result = response.body.result;
+          this.isFailed = false;
+        } else {
+          this.result = {};
+          this.isFailed = true;
+          this.error = response.body.error;
+        }
+
         this.isFetching = false;
-        this.isFailed = false;
       }, (response) => {
         // error callback
-        this.result = null;
+        this.result = {};
         this.isFetching = false;
         this.isFailed = true;
       });
